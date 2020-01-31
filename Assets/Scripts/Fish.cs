ffd19a9b4e5c0e5b37a74e4a1b10a15f0834d251
nataliap13 <nataliapop@op.pl>
2020-01-31 21:23:37 +0100

    failed build

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -9,13 +9,14 @@ public class Fish : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(TimerToHungry(5));
     }
 
     const int maxHappiness = 100;
     int hapiness = 50;
     public typeOfFishEnum typeOfFish = typeOfFishEnum.peaceful;
     public sizeOfFishEnum sizeOfFish = sizeOfFishEnum.small;
+    private Fish TargetFishToEat;
 
     //minNumberOfFishToHapiness of my type/spiecies
     public int minNumberOfFishToHapiness = 1;
@@ -26,12 +27,48 @@ public class Fish : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Move();
+        MoveHorizontal();
+        if(TargetFishToEat != null)
+        {
+            float step = speed * Time.deltaTime;
+
+            // move sprite towards the target location
+            var target = TargetFishToEat.transform.localPosition;
+            print(target);
+            var globalTargetPosition = transform.TransformPoint(TargetFishToEat.transform.localPosition);
+            transform.localPosition = Vector2.MoveTowards(transform.localPosition, new Vector2(transform.position.x, globalTargetPosition.y), step);
+        }
     }
 
-    public void Move()
+    private IEnumerator TimerToHungry(int timeInSeconds)
     {
-        gameObject.transform.Translate(speed * Time.deltaTime, 0, 0);
+        yield return new WaitForSeconds(timeInSeconds);
+        ChooseFishToEat();
+    }
+
+    private void ChooseFishToEat()
+    {
+        var fish = FindObjectsOfType<Fish>();
+        
+        foreach (var f in fish)
+        {
+            if(f.sizeOfFish < sizeOfFish)
+            {
+                TargetFishToEat = f;
+                break;
+            }
+        }
+    }
+
+    public void MoveHorizontal()
+    {
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+    }
+
+    public void MoveVertical()
+    {
+        
+        transform.Translate(0, speed * Time.deltaTime, 0);
     }
 
     public void Flip ()
