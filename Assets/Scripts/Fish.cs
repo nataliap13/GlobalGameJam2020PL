d62cee52ab02b34173d59dd9888a6793d15ea9db
nataliap13 <nataliapop@op.pl>
2020-01-31 19:31:33 +0100

    fish no. 1

---
+++ Assets/Scripts/Fish.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum typeOfFishEnum { peaceful, aggresive }
+public enum sizeOfFishEnum { small, medium, big }
+public class Fish : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    const int maxHappiness = 100;
+    int hapiness = 50;
+    public typeOfFishEnum typeOfFish = typeOfFishEnum.peaceful;
+    public sizeOfFishEnum sizeOfFish = sizeOfFishEnum.small;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
